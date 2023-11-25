0c795054c8a09c12d38dfe0f8e78dabb30c022b6
TimMedin <timmedin180@gmail.com>
2023-11-25 16:54:52 +0200

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+// task one
+let result = "5+5+`5`"
+console.log("Task 1");
+console.log(result);
+console.log(typeof result);
+
+// task two
+const email = "timmedin@gmail.com"
+console.log("Task 2");
+console.log(email.includes('@'));
+console.log(email.length);
+
+// task three
+const my = "My"
+const name = "name"
+const is = "is"
+console.log("Task 3");
+const FullName = my + name + is + "Victor"
+console.log(FullName);
+
+// task four
+let userName = "Олександра"
+let payment = "300"
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`)
+console.log("Task 4");
